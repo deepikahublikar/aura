5339ca150027e82f908aa3c3c4028f6e112c1f94
Anuj Datar <anuj.datar@gmail.com>
2020-09-19 12:53:08 +0530

    added about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,15 +1,32 @@
 <template>
-  <v-container>
-    <h1>About US</h1>
-  </v-container>
+  <v-layout
+    column
+    justify-center
+    align-center
+    style="width: 100%; padding: 0; margin: 0"
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      About Page
+    </v-flex>
+    <div
+      style="width: 100%; padding: 0; margin: 0"
+    >
+      <carousel />
+    </div>
+    <!-- <carousel /> -->
+  </v-layout>
 </template>
 
 <script>
 export default {
-
+  head () {
+    return {
+      title: 'About'
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
