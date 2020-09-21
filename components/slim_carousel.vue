96184f22231b328fe7ee36182f2e6f118f853840
Anuj Datar <anuj.datar@gmail.com>
2020-09-22 00:29:28 +0530

    slim carousel element for other pages, slimmer images, bare component, needs more work

---
+++ components/slim_carousel.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-carousel
+    style="margin: 0; padding: 0; width: 100%"
+    cycle
+    show-arrows-on-hover
+    hide-delimiter-background
+  >
+    <v-carousel-item
+      v-for="(item, i) in images"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    >
+      <!-- <v-img :src="item.src" /> -->
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      images: [
+        { src: require('~/assets/images/slim_carousel/banner01a.jpg') },
+        { src: require('~/assets/images/slim_carousel/banner02a.jpg') },
+        { src: require('~/assets/images/slim_carousel/banner03a.jpg') },
+        { src: require('~/assets/images/slim_carousel/banner04a.jpg') },
+        { src: require('~/assets/images/slim_carousel/banner05a.jpg') },
+        { src: require('~/assets/images/slim_carousel/banner06a.jpg') },
+        { src: require('~/assets/images/slim_carousel/banner07a.jpg') },
+        { src: require('~/assets/images/slim_carousel/banner08a.jpg') }
+      ]
+    }
+  }
+}
+</script>
